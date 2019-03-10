5d6491c0ccead94c34626bfcc5704d0300792739
Ashlyn Rains <ashlyntheis91@gmail.com>
2019-03-10 17:14:44 -0500

    added comments

--- project1.js
+++ project1.js
@@ -6,19 +6,19 @@ const submitHandler = event => {
     //Declaring consts.
     const weight = document.querySelector('#weight').value;
     const date = document.querySelector('#date').value;
-
-    const weightInfo = `${weight} ${date}`;
-    console.log(weightInfo);
+    //declaring table elements.
     const tableRow = document.createElement("tr");
     const weightCell = document.createElement("td");
     const dateCell = document.createElement("td");
+    //filling out table elements inserting the form data.
     weightCell.append(weight);
     dateCell.append(date);
     tableRow.appendChild(weightCell);
     tableRow.appendChild(dateCell);
+    //appending table row to main table.
     table.appendChild(tableRow);
 
 
 }
-
+//registering form eventlistener.
 form.addEventListener('submit', submitHandler);
